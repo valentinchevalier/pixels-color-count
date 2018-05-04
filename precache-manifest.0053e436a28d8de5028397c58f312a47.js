90d877397a94fa2a6b41e2ff24f16ff55e5020f1
Valentin Chevalier <valentinchevalier@munso.co>
2018-05-04 15:05:49 +0200

    Clean webpack config for html

---
+++ precache-manifest.0053e436a28d8de5028397c58f312a47.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = [
+  {
+    "revision": "4dd436c4a8375ca7713f318179ffd0af",
+    "url": "d3e346bd0f7314d8410a.worker.js"
+  },
+  {
+    "revision": "641a5dbcd53bd7448121",
+    "url": "app.bundle.js"
+  },
+  {
+    "revision": "641a5dbcd53bd7448121",
+    "url": "app.bundle.css"
+  }
+];
