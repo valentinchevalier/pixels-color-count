f2f2a0166593c204b4b8e137d8b1a0223a275941
Valentin Chevalier <valentinchevalier@munso.co>
2018-05-04 16:39:20 +0200

    Debug

--- src/index.js
+++ src/index.js
@@ -12,13 +12,13 @@ function domReady(callback) {
 domReady(() => {
   AnalysePixels.init();
 
-  // if ('serviceWorker' in navigator) {
-  //   window.addEventListener('load', () => {
-  //     navigator.serviceWorker.register('sw.js').then((registration) => {
-  //       console.log('SW registered: ', registration);
-  //     }).catch((registrationError) => {
-  //       console.log('SW registration failed: ', registrationError);
-  //     });
-  //   });
-  // }
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('sw.js').then((registration) => {
+        console.log('SW registered: ', registration);
+      }).catch((registrationError) => {
+        console.log('SW registration failed: ', registrationError);
+      });
+    });
+  }
 });
